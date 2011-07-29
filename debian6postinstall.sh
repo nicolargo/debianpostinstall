23d6becb0f8697a0ba7360318e78c17622298e9b
nicolargo <nicolargo@pcdesktopboulot.alcasat.net>
2011-07-29 11:46:08 +0200

    Add Vim configuration

--- debian6postinstall.sh
+++ debian6postinstall.sh
@@ -7,7 +7,7 @@
 # Syntaxe: # su - -c "./debian6postinstall.sh"
 # Syntaxe: or # sudo ./debian6postinstall.sh
 
-VERSION="1.61"
+VERSION="1.62"
 
 #=============================================================================
 # Liste des applications installés par le script
@@ -61,7 +61,7 @@ LISTE=$LISTE" shutter libgoo-canvas-perl"
 LISTE=$LISTE" openvpn resolvconf network-manager-openvpn-gnome"
 
 # Dev
-LISTE=$LISTE" subversion git anjuta python-rope"
+LISTE=$LISTE" subversion git vim anjuta python-rope"
 TEXTADEPT_VERSION="3.9"
 
 #=============================================================================
