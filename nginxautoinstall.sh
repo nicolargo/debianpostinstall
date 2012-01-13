dc302739fbfacbf9fdd985ab5270b8a4bd2b1051
nicolargo <nicolargo@nicolargo-boulot.(none)>
2012-01-13 14:04:21 +0100

    Change version number

--- nginxautoinstall.sh
+++ nginxautoinstall.sh
@@ -9,13 +9,14 @@
 # Syntaxe: # su - -c "./nginxautoinstall.sh"
 # Syntaxe: or # sudo ./nginxautoinstall.sh
 #
-VERSION="1.40"
+VERSION="1.41"
 
 ##############################
 # Version de NGinx a installer
 
-#NGINX_VERSION="0.8.54" # The legacy version
-NGINX_VERSION="1.1.11"   # The stable version
+#NGINX_VERSION="0.8.55" # The legacy version
+#NGINX_VERSION="1.0.11" # The stable version
+NGINX_VERSION="1.1.12"   # The development version
 
 ###############################
 # Liste des modules a installer
