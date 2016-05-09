887456da2ba1e03dad4659024a91033d8836516f
Bertrand <chikoumi54@gmail.com>
2016-05-09 14:35:39 +0200

    Update nginxautoinstall.sh
    + change to nginx-1.10.0 stable version that has been released
    + change pagespeed to 1.11.33.1.

--- nginxautoinstall.sh
+++ nginxautoinstall.sh
@@ -34,14 +34,14 @@ WITH_PAGESPEED="TRUE"
 
 # Current NGinx version
 # See: http://nginx.org/en/download.html
-NGINX_LEGACY_VERSION="1.6.3"
-NGINX_STABLE_VERSION="1.8.0"
-NGINX_DEV_VERSION="1.9.2"
+NGINX_LEGACY_VERSION="1.8.1"
+NGINX_STABLE_VERSION="1.10.0"
+NGINX_DEV_VERSION="1.9.15"
 
 # PageSpeed version
 # https://github.com/pagespeed/ngx_pagespeed/releases
-PAGESPEED_VERSION="1.9.32.4-beta"
-PAGESPEED_PSOL_VERSION="1.9.32.4"
+PAGESPEED_VERSION="1.11.33.1-beta"
+PAGESPEED_PSOL_VERSION="1.11.33.1"
 PAGESPEED_CACHE_DIR="/var/ngx_pagespeed_cache"
 
 # Functions
